3c721be7ab9f8fb0edaa8c73a0b658aa8e7b9a9d
LilyKras <62222363+LilyKras@users.noreply.github.com>
2020-12-09 15:48:57 +0300

    Update shell_sort.cpp

--- shell_sort.cpp
+++ shell_sort.cpp
@@ -28,26 +28,26 @@ int main(){
         fin >> a[i];
     }
     fin.close();
-    
+
     ofstream fout;
     fout.open("output.txt");
-    
+
     for (int i = 0; i < n; i++) {
         fout << a[i] << " ";
     }
     fout << endl;
-    
+
     unsigned int start_time =  clock();
-    shell_sort(a, n); 
-    unsigned int end_time = clock(); 
+    shell_sort(a, n);
+    unsigned int end_time = clock();
     unsigned int search_time = end_time - start_time;
-    
+
     for (int i = 0; i < n; i++) {
         fout << a[i] << " ";
     }
     fout << endl;
     fout << search_time << endl;
-    
+
     return 0;
 
 }
