8b3974e048aaec91f240eb5f9246a610c5ab3b98
LilyKras <62222363+LilyKras@users.noreply.github.com>
2020-12-09 18:03:06 +0700

    Create check.py

---
+++ check.py
@@ -0,0 +1,38 @@
+import random
+import subprocess
+
+print("1 shellsort\n2 quicksort\n3 heapsort\n")
+ans = int(input())
+sort_name=""
+if (ans==1):
+    sort_name="shell_sort"
+elif(ans==2):
+    sort_name="qsort"
+elif(ans==3):
+    sort_name="heap_sort"
+count=0
+time_arr = []
+tests=100
+
+for i in range(tests):
+    n = random.randint(1, 2000)
+    file = open("input.txt", 'w')
+    file.write(str(n)+"\n")
+    for j in range(n):
+        file.write(str(random.randint(-2000000000, 2000000000))+" ")
+    file.write("\n")
+    file.close()
+    #subprocess.run([sort_name + '.exe', '-o', sort_name + '.o'])
+    file = open("output.txt", 'r')
+    start_values  = list(map(int, file.readline().split()))
+    result_values = list(map(int, file.readline().split()))
+    time = int(file.readline())
+    file.close()
+    
+    sort_arr = sorted(list(map(int, start_values)))
+    if(sort_arr == result_values):
+        count +=1
+        time_arr.append(time)
+        
+print("Correct:", count, "/", tests,"\n")
+print(*time_arr)
